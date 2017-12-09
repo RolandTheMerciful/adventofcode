a99d99cbd10c935617d4a90ff18be8dac586fc95
Roland <roland.bair@gmail.com>
2017-12-10 00:45:56 +0100

    Day3 - part a basic

--- Day3.elm
+++ Day3.elm
@@ -1,37 +1,61 @@
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+module Hello exposing (..)
 
+import Html exposing (text)
+
+--input = 9
+input = 265149
+
+{--  test function
+1: 0,0,0 --> 1,0,1
+2: 1,0,1 --> 1,1,1
+3: 1,1,1 --> 0,1,1
+4: 0,1,1 --> -1,1,1
+5: -1,1,1 --> -1,0,1
+6: -1,0,1 --> -1,-1,1
+7: -1,-1,1 --> 0,-1,1
+8: 0,-1,1 --> 1,-1,1
+--}
+
+addItemA (currX, currY, borderDistToCenter, currVal, expectedVal) = 
+    let
+        nextVal = currVal + 1
+    in
+        if (currX == borderDistToCenter) && (currY == -borderDistToCenter) && (currVal < expectedVal) then --move out
+            addItemA (currX + 1, currY, borderDistToCenter + 1, nextVal, expectedVal)
+        else if (currX == borderDistToCenter) && (currY < borderDistToCenter) && (currVal < expectedVal) then --move up
+            addItemA (currX, currY + 1, borderDistToCenter, nextVal, expectedVal)
+        else if (currX > -borderDistToCenter) && (currY == borderDistToCenter) && (currVal < expectedVal) then --move left
+            addItemA (currX - 1, currY, borderDistToCenter, nextVal, expectedVal)
+        else if (currX == -borderDistToCenter) && (currY > -borderDistToCenter) && (currVal < expectedVal) then --move down
+            addItemA (currX, currY - 1, borderDistToCenter, nextVal, expectedVal)
+        else if (currX < borderDistToCenter) && (currY == -borderDistToCenter) && (currVal < expectedVal) then --move right
+            addItemA (currX + 1, currY, borderDistToCenter, nextVal, expectedVal)
+        else
+            "currVal: " ++ (toString currVal) ++ " currX: " ++ (toString currX) ++ " currY: " ++ (toString currY) ++ " borderDistToCenter: " ++ (toString borderDistToCenter)
+
+addItemB (currX, currY, borderDistToCenter, currVal, expectedVal) = 
+    let
+        max = (borderDistToCenter * 2) + 1
+        nextVal = currVal + 1
+    in
+        if (currX == borderDistToCenter) && (currY == -borderDistToCenter) && (currVal < expectedVal) then --move out
+            addItemB (currX + 1, currY, borderDistToCenter + 1, nextVal, expectedVal)
+        else if (currX == borderDistToCenter) && (currY < borderDistToCenter) && (currVal < expectedVal) then --move up
+            addItemB (currX, currY + 1, borderDistToCenter, nextVal, expectedVal)
+        else if (currX > -borderDistToCenter) && (currY == borderDistToCenter) && (currVal < expectedVal) then --move left
+            addItemB (currX - 1, currY, borderDistToCenter, nextVal, expectedVal)
+        else if (currX == -borderDistToCenter) && (currY > -borderDistToCenter) && (currVal < expectedVal) then --move down
+            addItemB (currX, currY - 1, borderDistToCenter, nextVal, expectedVal)
+        else if (currX < borderDistToCenter) && (currY == -borderDistToCenter) && (currVal < expectedVal) then --move right
+            addItemB (currX + 1, currY, borderDistToCenter, nextVal, expectedVal)
+        else
+            "currVal: " ++ (toString currVal) ++ " currX: " ++ (toString currX) ++ " currY: " ++ (toString currY) ++ " borderDistToCenter: " ++ (toString borderDistToCenter)
+
+--partA: retVal = addItemA (0,0,0,1,input)
+
+retVal = addItemB (0,0,0,1,input)
+
+    
 main =
-  Html.beginnerProgram { model = model, view = view, update = update }
-
--- MODEL
-
-type alias Model = Int
-
-model : Model
-model =
-  0
-  
--- UPDATE
-
-type Msg = Increment | Decrement
-
-update : Msg -> Model -> Model
-update msg model =
-  case msg of
-    Increment ->
-      model + 2
-
-    Decrement ->
-      model - 1
-
-
--- VIEW
-
-view : Model -> Html Msg
-view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (toString model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]
+    --text "hello"
+    text <| toString retVal
